c7e727d57b788eaf612abe9e8e80723e5fadb1a3
mizuki <trombonelover8888@gmail.com>
2023-06-08 01:26:25 +0900

    [fix] some bug backend file, change docker image

--- backend/app/main.py
+++ backend/app/main.py
@@ -2,7 +2,10 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-
 @app.get("/")
-async def root():
-    return {"message": "Hello World"}
+def root():
+    return {"message": "H"}
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: str = None):
+    return {"item_id": item_id, "q": q}
