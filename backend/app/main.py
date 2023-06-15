1141c7ede81cd5e9b6bfb7fda386d80e92e4b0c5
arara1930 <kouhei00ara@gmail.com>
2023-06-15 22:32:41 +0900

    [refactar]Supports integration with databases

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,34 +1,47 @@
 from fastapi import FastAPI
-from pydantic import BaseModel
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import firestore
+
 
 app = FastAPI()
 
 @app.get("/")
 def root():
-    return {"message": "H"}
+    return {"NetworkImage":{"url":"https://www.notion.so/6-16-698927660ec446c5af6b7dc62a9f8d3c"},
+            "name":"荒沢康平",
+            "school":"静岡大学",
+            "match-leval":"80",
+            "AI_degree":"20", 
+            "wrap-up":[{"genre":"志望動機",
+                        "content":"金が欲しいから"},
+                        {"genre":"技術スタック",
+                        "content":"なし"}],
+            "questions":[{"genre":"志望動機",
+                        "content":"就活なめてんのか"},
+                        {"genre":"技術スタック",
+                        "content":"なめとんのかわれ"}]
+            }
 
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: str = None):
     return {"item_id": item_id, "q": q}
 
-class Input(BaseModel):
-    user_id: str
-    query: str
+# Firebase Admin SDKの初期化
+cred = credentials.Certificate("admin_info.json")
+firebase_admin.initialize_app(cred)
 
-class Output(BaseModel):
-    user_id: str
-    query: str
-    output: str
+# Firestoreクライアントの作成
+db = firestore.client()
 
-@app.post("/process")
-def main(input_data: Input):
-    # 入力データを取得
-    user_id = input_data.user_id
-    query = input_data.query
 
-    # 文字列の加工処理
-    processed_text = f"Processed _ User ID: {user_id}, Query: {query}"
+@app.get("/data")
+async def get_data():
+    data = []
+    # Firestoreからデータを取得する処理
+    collection_ref = db.collection("student")
+    docs = collection_ref.get()
+    for doc in docs:
+        data.append(doc.to_dict())
 
-    # 処理結果と入力データをまとめて返す
-    output_data = Output(user_id=user_id, query=query, output=processed_text)
-    return output_data
+    return {"data": data}
