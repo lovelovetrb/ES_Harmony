f40ec8c094920e4cd6fb1f602d57383d5b470651
mizuki <trombonelover8888@gmail.com>
2023-06-19 14:52:24 +0900

    [add] can fetch backend

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import firestore
@@ -6,13 +7,27 @@ from firebase_admin import firestore
 
 app = FastAPI()
 
+origins = [
+    "http://localhost",
+    "http://localhost:3000",
+    "http://frontend:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 @app.get("/")
 def root():
-    return {"NetworkImage":{"url":"https://www.notion.so/6-16-698927660ec446c5af6b7dc62a9f8d3c"},
+    return [{"NetworkImage":{"url":"https://www.notion.so/6-16-698927660ec446c5af6b7dc62a9f8d3c"},
             "name":"荒沢康平",
             "school":"静岡大学",
-            "match-leval":"80",
-            "AI_degree":"20", 
+            "match_level":80,
+            "AI_degree":20,
             "wrap-up":[{"genre":"志望動機",
                         "content":"金が欲しいから"},
                         {"genre":"技術スタック",
@@ -21,10 +36,10 @@ def root():
                         "content":"就活なめてんのか"},
                         {"genre":"技術スタック",
                         "content":"なめとんのかわれ"}]
-            }
+            }]
 
 @app.get("/items/{item_id}")
-def read_item(item_id: int, q: str = None):
+def read_item(item_id: int, q: str):
     return {"item_id": item_id, "q": q}
 
 # Firebase Admin SDKの初期化
