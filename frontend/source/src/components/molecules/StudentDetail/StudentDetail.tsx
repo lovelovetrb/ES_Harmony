c8cc072ac3f476f2a7f1151c6f50ca8efd46393a
mizuki <trombonelover8888@gmail.com>
2023-06-23 03:03:59 +0900

    [add] detail page

--- frontend/source/src/components/molecules/StudentDetail/StudentDetail.tsx
+++ frontend/source/src/components/molecules/StudentDetail/StudentDetail.tsx
@@ -9,32 +9,19 @@ export default function StudentDetail({ data }: props) {
     return (
         <div className={styles.container}>
             <main className={styles.main}>
-                <div className={styles.heading}>{data.name} さんのES</div>
+                <div className={styles.nameArea}>{data.name} さんのES</div>
                 <div className={styles.inner}>
                     <div className={styles.box}>
                         <p>{data.match_level}%マッチ</p>
-                        <p>Ai度：{data.AI_degree}%</p>
+                        <p>Ai度:{data.AI_degree}%</p>
                     </div>
                     <div className={styles.wrap_up}>
-                        <h2>要約</h2>
-                        {data.wrap_up.map((item: WrapUp, index: number) => {
-                            return (
-                                <div key={index}>
-                                    <h3>
-                                        {index + 1}. ジャンル：{item.genre}
-                                    </h3>
-                                    <h3>・内容：{item.content}</h3>
-                                </div>
-                            );
-                        })}
-                    </div>
-                    <div className={(styles.flex_column)}>
-                        <div className={styles.wrap_up}>
-                            <h2>要約</h2>
+                        <h2 className={styles.heading}>要約</h2>
+                        <div className={styles.textArea}>
                             {data.wrap_up.map((item: WrapUp, index: number) => {
                                 return (
                                     <div key={index}>
-                                        <h3>
+                                        <h3 className={styles.subheading}>
                                             {index + 1}. ジャンル：{item.genre}
                                         </h3>
                                         <h3>・内容：{item.content}</h3>
@@ -42,18 +29,36 @@ export default function StudentDetail({ data }: props) {
                                 );
                             })}
                         </div>
+                    </div>
+                    <div className={styles.flex_column}>
+                        <div className={styles.imageArea}>
+                            <h2 className={styles.heading}>ネットワーク図</h2>
+                            <div className={styles.textArea}>写真</div>
+                        </div>
                         <div className={styles.question}>
-                            <h2>質問</h2>
-                            {data.questions.map((item: Question, index: number) => {
-                                return (
-                                    <div key={index}>
-                                        <h3>
-                                            {index + 1}. ジャンル：{item.genre}
-                                        </h3>
-                                        <h3>・内容：{item.content}</h3>
-                                    </div>
-                                );
-                            })}
+                            <h2 className={styles.heading}>質問</h2>
+                            <div className={styles.textArea}>
+                                {data.questions.map((item: Question, index: number) => {
+                                    return (
+                                        <div key={index}>
+                                            <h3 className={styles.subheading}>
+                                                {index + 1}. ジャンル：{item.genre}
+                                            </h3>
+                                            <h3>・内容：{item.content}</h3>
+                                        </div>
+                                    );
+                                })}
+                            </div>
+                        </div>
+                    </div>
+                    <div className={styles.ablityComposeText}>
+                        <h2 className={styles.heading}>文章構力分析</h2>
+                        <div className={styles.textArea}>
+                            <p dangerouslySetInnerHTML={data.ability_to_compose_a_text} />
+                            <ol className={styles.usage}>
+                                <li className="highlight">オリジナリティのある文章</li>
+                                <li className="ai_highlight">AIによる生成の可能性のある文章</li>
+                            </ol>
                         </div>
                     </div>
                 </div>
