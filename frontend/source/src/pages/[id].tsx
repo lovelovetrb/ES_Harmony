19a0269d078e9fefc25805db4298b5998bce1443
mizuki <trombonelover8888@gmail.com>
2023-06-23 01:12:18 +0900

    [add] animation

--- frontend/source/src/pages/[id].tsx
+++ frontend/source/src/pages/[id].tsx
@@ -1,42 +1,41 @@
 import Header from "@/components/Header/Header";
 import StudentDetail from "@/components/molecules/StudentDetail/StudentDetail";
 
-import { GetStaticProps } from "next";
+import Link from "next/link";
+
+import { motion } from "framer-motion";
 
 import { StudentData } from "@/types/types";
 import { getData } from "@/lib/getData";
-
 
 type Props = {
     data: StudentData;
 };
 
 const id = ({ data }: Props) => {
-    {
-        /* TDOO: dataの差し替え */
-    }
     return (
         <>
-            <Header />
-            <StudentDetail data={data} />
+            <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1 }} exit={{ opacity: 0 }} transition={{ duration: 0.15 }}>
+                <Header />
+                <StudentDetail data={data} />
+                <Link href="/" />
+            </motion.div>
         </>
     );
 };
 
 export const getStaticPaths = async () => {
     const studentData: StudentData[] = await getData();
-    const paths = studentData.map((data) => ({
+    const paths = studentData.map((data: StudentData) => ({
         params: { id: data.id },
     }));
-    return { paths, fallback: "blocking" };
+    return { paths, fallback: false };
 };
 
-export const getStaticProps: GetStaticProps<{
-    data: StudentData;
-}> = async () => {
-    const data: StudentData[] = await getData();
+export const getStaticProps = async ({ params }: string) => {
+    const data: StudentData = await getData(params.id);
     return {
-        props: { data: data[0] },
+        props: { data: data },
         revalidate: 60,
     };
 };
--- frontend/source/src/pages/_app.tsx
+++ frontend/source/src/pages/_app.tsx
@@ -1,8 +1,13 @@
 import "@/styles/globals.css";
 import "@/styles/destyle.css";
+
 import type { AppProps } from "next/app";
+import { AnimatePresence } from "framer-motion";
 
-
-export default function App({ Component, pageProps }: AppProps) {
-    return <Component {...pageProps} />;
+export default function App({ Component, pageProps, router }: AppProps) {
+    return (
+        <AnimatePresence mode="wait" onExitComplete={() => window.scrollTo(0, 0)}>
+            <Component key={router.asPath} {...pageProps} />
+        </AnimatePresence>
+    );
 }
