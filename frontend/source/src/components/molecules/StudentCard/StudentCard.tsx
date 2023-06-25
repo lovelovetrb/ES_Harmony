83caf6f91aa727b251c0ffa424f436a960a2d34b
arara1930 <kouhei00ara@gmail.com>
2023-06-26 01:42:24 +0900

    [add]degree(S,A,B,C)

--- frontend/source/src/components/molecules/StudentCard/StudentCard.tsx
+++ frontend/source/src/components/molecules/StudentCard/StudentCard.tsx
@@ -8,6 +8,7 @@ import { useGetElementProperty } from "@/lib/hooks";
 
 import { StudentData } from "@/types/types";
 
+
 type props = {
     studentData: StudentData;
 };
@@ -26,15 +27,49 @@ const StudentCard = ({ studentData }: props) => {
                     <Image src="/icon.png" height={40} width={40} alt="icon" />
                 </div>
                 <div className={styles.student_info}>
-                    <h3>{studentData.name} さん</h3>
-                    <h3>{studentData.school}</h3>
+                    <h3>🧑‍🎓{studentData.name} さん</h3>
+                    <h3>🏫{studentData.school}</h3>
                 </div>
                 <div className={styles.match_info}>
-                    <h3>マッチ度:{studentData.match_level}</h3>
-                    <h3>AI度:{studentData.AI_degree}</h3>
+                    <h3>📌マッチ度:{studentData.match_level}</h3>
+                    {/* 一番外側の中括弧はJSXに式を埋め込むためのもの */}
+                    {/* 一番外側の中括弧で囲まれた部分が即時関数 */}
+                    {(() => {
+                        if (studentData.match_level > 50) {
+                            if (studentData.match_level > 75) {
+                                return <p className={styles.evalSwitch_S}>S{studentData.match_level}</p>;
+                            } else {
+                                return <p className={styles.evalSwitch_A}>A{studentData.match_level}</p>;
+                            }
+                        } else {
+                            if (studentData.match_level > 25) {
+                                return <p className={styles.evalSwitch_B}>B{studentData.match_level}</p>;
+                            } else {
+                                return <p className={styles.evalSwitch_C}>C{studentData.match_level}</p>;
+                            }
+                        }
+                    })()}
+                    <h3>📌AI度:{studentData.AI_degree}</h3>
+                    {/* 一番外側の中括弧はJSXに式を埋め込むためのもの */}
+                    {/* 一番外側の中括弧で囲まれた部分が即時関数 */}
+                    {(() => {
+                        if (studentData.AI_degree > 50) {
+                            if (studentData.AI_degree > 75) {
+                                return <p className={styles.evalSwitch_S}>S{studentData.AI_degree}</p>;
+                            } else {
+                                return <p className={styles.evalSwitch_A}>A{studentData.AI_degree}</p>;
+                            }
+                        } else {
+                            if (studentData.AI_degree > 25) {
+                                return <p className={styles.evalSwitch_B}>B{studentData.AI_degree}</p>;
+                            } else {
+                                return <p className={styles.evalSwitch_C}>C{studentData.AI_degree}</p>;
+                            }
+                        }
+                    })()}
                 </div>
                 <div className={styles.judgeArea}>
-                    <p>詳細を見る→</p>
+                    <p>👀詳細を見る→</p>
                 </div>
             </div>
         </Link>
