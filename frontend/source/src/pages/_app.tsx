622454682495dea1f2415f2863980cf0ff890902
mizuki <trombonelover8888@gmail.com>
2023-06-28 21:41:13 +0900

    [add] layout.tsx

--- frontend/source/src/pages/_app.tsx
+++ frontend/source/src/pages/_app.tsx
@@ -3,11 +3,14 @@ import "@/styles/destyle.css";
 
 import type { AppProps } from "next/app";
 import { AnimatePresence } from "framer-motion";
+import { Layout } from "@/components/Layout";
 
 export default function App({ Component, pageProps, router }: AppProps) {
     return (
         <AnimatePresence mode="wait" onExitComplete={() => window.scrollTo(0, 0)}>
-            <Component key={router.asPath} {...pageProps} />
+            <Layout>
+                <Component key={router.asPath} {...pageProps} />
+            </Layout>
         </AnimatePresence>
     );
 }
