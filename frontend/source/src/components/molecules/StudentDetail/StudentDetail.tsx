aeff7d0780f34bfbcfcbaa0304b8c223a821a861
Mizuki <mbaba@kanolab.net>
2023-07-05 23:17:09 +0900

    [fix] backend port

--- frontend/source/src/components/molecules/StudentDetail/StudentDetail.tsx
+++ frontend/source/src/components/molecules/StudentDetail/StudentDetail.tsx
@@ -16,6 +16,45 @@ export default function StudentDetail({ data }: props) {
                     <div className={styles.box}>
                         <p>{data.match_level}%マッチ📌</p>
                     </div>
+                    <div className={styles.ablityComposeText}>
+                        <h2 className={styles.heading}>📝オリジナリティ分析 / オリジナリティ:{data.originality}%</h2>
+                        <div className={styles.textArea}>
+                            <div className={styles.analyzeArea} dangerouslySetInnerHTML={data.ability_to_compose_a_text} />
+                            <ol className={styles.usage}>
+                                <li className="highlight">オリジナリティのある文章</li>
+                                <li className="ai_highlight">AIによる生成の可能性のある文章</li>
+                            </ol>
+                        </div>
+                    </div>
+                    <div className={styles.flex_column}>
+                        <div className={styles.imageArea}>
+                            <h2 className={styles.heading}>🌍 ネットワーク図</h2>
+                            <div className={styles.textArea}>
+                                <img src={data.NetworkImage.url} alt="ネットワーク図" />
+                            </div>
+                        </div>
+                        <div className={styles.imageArea}>
+                            <h2 className={styles.heading}>🌍 ネットワーク図</h2>
+                            <div className={styles.textArea}>
+                                <img src={data.NetworkImage.url} alt="ネットワーク図" />
+                            </div>
+                        </div>
+                    </div>
+                    <div className={styles.question}>
+                        <h2 className={styles.heading}>🧑🏻‍💻質問</h2>
+                        <div className={styles.textArea}>
+                            {data.questions.map((item: Question, index: number) => {
+                                return (
+                                    <div key={index}>
+                                        <h3 className={styles.subheading}>
+                                            {index + 1}. ジャンル：{item.genre}
+                                        </h3>
+                                        <h3>・内容：{item.content}</h3>
+                                    </div>
+                                );
+                            })}
+                        </div>
+                    </div>
                     <div className={styles.wrap_up}>
                         <h2 className={styles.heading}>🤩要約</h2>
                         <div className={styles.textArea}>
@@ -26,39 +65,6 @@ export default function StudentDetail({ data }: props) {
                                     </div>
                                 );
                             })}
-                        </div>
-                    </div>
-                    <div className={styles.flex_column}>
-                        <div className={styles.imageArea}>
-                            <h2 className={styles.heading}>🌍 ネットワーク図</h2>
-                            <div className={styles.textArea}>
-                                <img src={data.NetworkImage.url} alt="ネットワーク図" />
-                            </div>
-                        </div>
-                        <div className={styles.question}>
-                            <h2 className={styles.heading}>🧑🏻‍💻質問</h2>
-                            <div className={styles.textArea}>
-                                {data.questions.map((item: Question, index: number) => {
-                                    return (
-                                        <div key={index}>
-                                            <h3 className={styles.subheading}>
-                                                {index + 1}. ジャンル：{item.genre}
-                                            </h3>
-                                            <h3>・内容：{item.content}</h3>
-                                        </div>
-                                    );
-                                })}
-                            </div>
-                        </div>
-                    </div>
-                    <div className={styles.ablityComposeText}>
-                        <h2 className={styles.heading}>📝オリジナリティ分析 / オリジナリティ:{data.originality}%</h2>
-                        <div className={styles.textArea}>
-                            <div className={styles.analyzeArea} dangerouslySetInnerHTML={data.ability_to_compose_a_text} />
-                            <ol className={styles.usage}>
-                                <li className="highlight">オリジナリティのある文章</li>
-                                <li className="ai_highlight">AIによる生成の可能性のある文章</li>
-                            </ol>
                         </div>
                     </div>
                 </div>
--- frontend/source/src/lib/getData.ts
+++ frontend/source/src/lib/getData.ts
@@ -1,7 +1,7 @@
 import { StudentData } from "@/types/types";
 
 export async function getData(id?: string) {
-    const url = id ? `http://backend:5000/data/${id}` : "http://backend:5000/data";
+    const url = id ? `http://backend:5002/data/${id}` : "http://backend:5002/data";
     const data: StudentData[] | StudentData = await fetch(url)
         .then((response) => response.json())
         .then((data) => data);
