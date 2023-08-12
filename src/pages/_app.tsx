34fe2abbf85271a00a11898a0058d56ccb8d77be
Mizuki <mbaba@kanolab.net>
2023-08-12 10:38:28 +0900

    [fix] tablet design

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,16 +5,16 @@ import type { AppProps } from "next/app";
 import { AnimatePresence } from "framer-motion";
 import { Layout } from "@/components/Layout";
 
-import { config } from '@fortawesome/fontawesome-svg-core'
-import '@fortawesome/fontawesome-svg-core/styles.css'
-config.autoAddCss = false
+import { config } from "@fortawesome/fontawesome-svg-core";
+import "@fortawesome/fontawesome-svg-core/styles.css";
+config.autoAddCss = false;
 
 export default function App({ Component, pageProps, router }: AppProps) {
-    return (
-        <AnimatePresence mode="wait" onExitComplete={() => window.scrollTo(0, 0)}>
-            <Layout>
-                <Component key={router.asPath} {...pageProps} />
-            </Layout>
-        </AnimatePresence>
-    );
+  return (
+    <AnimatePresence mode="wait" onExitComplete={() => window.scrollTo(0, 0)}>
+      <Layout>
+        <Component key={router.asPath} {...pageProps} />
+      </Layout>
+    </AnimatePresence>
+  );
 }
