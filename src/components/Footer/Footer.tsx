74bbf796d7060df34484b05680dca41f357641f7
Mizuki <mbaba@kanolab.net>
2023-08-12 10:57:09 +0900

    [fix] font

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,22 +1,20 @@
+import { Zen_Kaku_Gothic_New } from "next/font/google";
 import styles from "@/components/Footer/Footer.module.css";
-import { Noto_Sans_JP } from "next/font/google";
 
-const notojp = Noto_Sans_JP({
-  weight: ["400", "500", "700"],
+const zenkaku = Zen_Kaku_Gothic_New({
+  weight: ["500"],
   subsets: ["latin"],
-  variable: "--font-notojp",
-  display: "swap",
 });
 
 const Footer = () => {
-    return (
-        <div className={`${styles.wrapper} ${notojp.variable}`}>
-            <ul className={styles.list}>
-                <li>ES Harmonyについて</li>
-                <li>AI技術</li>
-            </ul>
-            <p className={styles.brand_name}>© 2023 ES Harmony</p>
-        </div>
-    );
+  return (
+    <div className={`${styles.wrapper} ${zenkaku.className}`}>
+      <ul className={styles.list}>
+        <li>ES Harmonyについて</li>
+        <li>AI技術</li>
+      </ul>
+      <p className={styles.brand_name}>© 2023 ES Harmony</p>
+    </div>
+  );
 };
 export default Footer;
